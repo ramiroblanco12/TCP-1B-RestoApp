9ab33da0e869e96c935b91d050982f9bd29048b6
TomasAvalos8 <tomasgonzaloavalos@gmail.com>
2024-11-24 21:21:51 -0300

    clase pedido

---
+++ Dominio1/Pedido.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio1
+{
+    internal class Pedido
+    {
+        public int Id { get; set; }
+        public int IdMozo { get; set; }
+        public int IdMesa { get; set; }
+        public decimal Monto { get; set; }
+        public DateTime Fecha { get; set; }
+
+
+
+    }
+}
